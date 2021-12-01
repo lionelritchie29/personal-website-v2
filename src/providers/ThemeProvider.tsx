2a680bfe6fe115a2d19c52e6c529f22023278bea
lionelritchie29 <lionelrtchieee@gmail.com>
2021-12-01 13:48:40 +0700

    fix: fetch theme initial value from local storage instead of using ThemeToggler

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,17 +1,20 @@
 import ThemeToggler from 'gatsby-plugin-dark-mode/ThemeToggler';
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 export const ThemeContext = createContext(null);
 
 const ThemeProvider: React.FC = ({ children }) => {
   const [theme, setTheme] = useState('light');
 
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      setTheme(localStorage.getItem('theme'));
+    }
+  }, []);
+
   return (
     <ThemeContext.Provider value={[theme, setTheme]}>
-      <ThemeToggler>
-        {({ theme, toggleTheme }) => <>{setTheme(theme)}</>}
-      </ThemeToggler>
-
       {children}
     </ThemeContext.Provider>
   );
